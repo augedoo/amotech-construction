9d6de951fdf0003d44b6fd90f22f61f86b7d5cf5
augedoo <47257672+augedoo@users.noreply.github.com>
2020-01-01 11:14:49 +0000

    Added tabllet media queries

--- js/main.js
+++ js/main.js
@@ -2,12 +2,12 @@
 window.addEventListener("scroll", function() {
   if (window.scrollY > 150) {
     document.querySelector("header").style.opacity = 0.9;
-    document.querySelector("header").style.padding = "0.5rem";
+    document.querySelector("header").style.padding = "1rem 0rem";
     document.querySelector("header").style.backgroundColor = "#272727";
     document.querySelector(".to-top-btn").style.opacity = 1;
   } else {
     document.querySelector("header").style.opacity = 1;
-    document.querySelector("header").style.padding = "1rem";
+    document.querySelector("header").style.padding = "2rem 0rem";
     document.querySelector("header").style.backgroundColor = "transparent";
     document.querySelector(".to-top-btn").style.opacity = 0;
   }
