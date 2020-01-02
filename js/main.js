f27847ab0f54f2af36ea7b4a02a2375f70986df0
augedoo <47257672+augedoo@users.noreply.github.com>
2020-01-02 11:34:24 +0000

    Added Mobile Versions

--- js/main.js
+++ js/main.js
@@ -1,15 +1,16 @@
 // Sticky menu background
+
 window.addEventListener("scroll", function() {
   if (window.scrollY > 150) {
     document.querySelector("header").style.opacity = 0.9;
     document.querySelector("header").style.padding = "1rem 0rem";
-    document.querySelector("header").style.backgroundColor = "#272727";
     document.querySelector(".to-top-btn").style.opacity = 1;
+    document.querySelector("header").style.background = "#272727";
   } else {
     document.querySelector("header").style.opacity = 1;
     document.querySelector("header").style.padding = "2rem 0rem";
-    document.querySelector("header").style.backgroundColor = "transparent";
     document.querySelector(".to-top-btn").style.opacity = 0;
+    document.querySelector("header").style.background = "transparent";
   }
 });
 
